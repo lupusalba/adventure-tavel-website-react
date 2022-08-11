260e91a222b017035eb70ac56f8e65b62b947505
lupusalba <a145296@gmail.com>
2022-08-11 22:21:28 +0200

    completed, all ok

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navigation from '../Components/Navigation'
 import Logo from '../Components/Logo'
+import Background from '../images/lost404.jpg'
 
 const Error = () => {
   return (
-    <div>
-      <h1>Error 404 page not found</h1>
+    <div className="onePage errorPage" style={{ backgroundImage:`url(${Background})` }}>
+      <div className="errorPageText">
+        <h1>You are lost mate!</h1>
+        <a className="btn-explore" href="/">This is the way Home</a>
+        <p className="errorPageNotFound">Error 404 page not found</p>
+      </div>
       <Logo />
       <Navigation />
     </div>
